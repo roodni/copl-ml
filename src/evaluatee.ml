83181a3eaf822d55c6ba881cea67e495ebd0a751
rodoni <41773436+roodni@users.noreply.github.com>
2020-04-12 02:18:13 +0900

    空の環境の出力を修正

--- src/evaluatee.ml
+++ src/evaluatee.ml
@@ -3,6 +3,6 @@ type t = { store : Store.t; env : Value.env; expr : Expr.expr }
 let to_string { store; env; expr } =
   let s_store =
     if Store.is_empty store then "" else Store.to_string store ^ " / "
-  and s_env = Value.env_to_string env ^ " |- "
+  and s_env = (if env <> [] then Value.env_to_string env ^ " " else "") ^ "|- "
   and s_expr = Expr.expr_to_string expr in
   s_store ^ s_env ^ s_expr
