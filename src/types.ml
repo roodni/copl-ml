f9104df4164d0edce7623b697ee0f68f19042b6a
rodoni <41773436+roodni@users.noreply.github.com>
2020-04-19 19:44:08 +0900

    list型は出力時に括弧不要

--- src/types.ml
+++ src/types.ml
@@ -6,4 +6,4 @@ let rec to_string = function
   | Bool -> "bool"
   | Int -> "int"
   | Fun (a, b) -> sprintf "(%s -> %s)" (to_string a) (to_string b)
-  | List t -> sprintf "(%s list)" (to_string t)
+  | List t -> sprintf "%s list" (to_string t)
