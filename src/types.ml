da79337b6e2b633de8ca0367ad112a161fd54f19
rodoni <41773436+roodni@users.noreply.github.com>
2020-04-24 17:52:00 +0900

    関数の型の出力の括弧を省略

--- src/types.ml
+++ src/types.ml
@@ -5,7 +5,11 @@ type t = Bool | Int | Fun of t * t | List of t | Var of Tvar.t
 let rec to_string = function
   | Bool -> "bool"
   | Int -> "int"
-  | Fun (a, b) -> sprintf "(%s -> %s)" (to_string a) (to_string b)
+  | Fun (a, b) ->
+      let a_s =
+        match a with Fun (_, _) -> "(" ^ to_string a ^ ")" | _ -> to_string a
+      in
+      sprintf "%s -> %s" a_s (to_string b)
   | List t -> sprintf "%s list" (to_string t)
   | Var t -> Tvar.to_string t
 
