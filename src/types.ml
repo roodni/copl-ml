5b381139946ae3bc6e4029d4cc78febbd21d3280
rodoni <41773436+roodni@users.noreply.github.com>
2020-04-24 22:19:52 +0900

    型出力時のlistと->の優先順位のバグを修正

--- src/types.ml
+++ src/types.ml
@@ -2,15 +2,19 @@ open Printf
 
 type t = Bool | Int | Fun of t * t | List of t | Var of Tvar.t
 
+let parenthesize s = sprintf "(%s)" s
+
 let rec to_string = function
   | Bool -> "bool"
   | Int -> "int"
   | Fun (a, b) ->
-      let a_s =
-        match a with Fun (_, _) -> "(" ^ to_string a ^ ")" | _ -> to_string a
-      in
+      let a_s = to_string a in
+      let a_s = match a with Fun (_, _) -> parenthesize a_s | _ -> a_s in
       sprintf "%s -> %s" a_s (to_string b)
-  | List t -> sprintf "%s list" (to_string t)
+  | List t ->
+      let s = to_string t in
+      let s = match t with Fun (_, _) -> parenthesize s | _ -> s in
+      sprintf "%s list" s
   | Var t -> Tvar.to_string t
 
 let compare a b = compare a b
